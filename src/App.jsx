14c21c9ae20538803a270495047ed2f81493ec18
mrbhanukab <mrbhanuka@outlook.com>
2023-03-26 21:41:59 +0530

    Ahh, Finally Weather Feature Perfectly Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import bg from "../src/assets/bg.jpeg";
+import Weather from "./components/weather";
 
 function App() {
   return (
     <section className="container">
       <div className="content">
-        
+        <Weather />
       </div>
-      <img src={bg} alt="Background Image" className="img" />
+      {/* <img src={bg} alt="Background Image" className="img" /> */}
     </section>
   );
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Weather.css";
+
 function Weather() {
+  const [City, setCity] = useState("Piliyandala"); // default City
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://api.weatherapi.com/v1/current.json?key=66c1121844014093bdd150031232603&q=${City}`
+      );
+      setWeatherData(result.data);
+    };
+    fetchData();
+  }, [City]);
+
   return (
-    <section className="container">
-      <div className="content"></div>
-      <img src={bg} alt="Background Image" className="img" />
-    </section>
+    <div className="weather">
+      <img src={weatherData.current?.condition?.icon} />
+      <div className="info">
+        <h6>{City}</h6>
+        <h3> {weatherData.current?.feelslike_c} °C</h3>
+      </div>
+    </div>
   );
 }
 
