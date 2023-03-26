d3d9f1138d1d576fe04b30ca432349ecd780b115
mrbhanukab <mrbhanuka@outlook.com>
2023-03-26 20:37:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import bg from "../src/assets/bg.jpeg";
+
+function App() {
+  return (
+    <section className="container">
+      <div className="content">
+        
+      </div>
+      <img src={bg} alt="Background Image" className="img" />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,10 @@
+function Weather() {
+  return (
+    <section className="container">
+      <div className="content"></div>
+      <img src={bg} alt="Background Image" className="img" />
+    </section>
+  );
+}
+
+export default Weather;
